73e3702d6f8d4c5a767f637776fdab2c49f9e7d4
prograTammz <ahmedtamer1998@gmail.com>
2020-02-01 06:25:52 +0200

    extended post and comments relation, one-many

--- src/index.js
+++ src/index.js
@@ -153,6 +153,9 @@ const resolvers = {
     Post: {
         author(parent, args, ctx,info){
             return users.find((user)=> user.id == parent.author)
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter((comment)=>comment.post == parent.id)
         }
     },
     User:{
